be982d41a1052749076bd49b1c3e4742e5898457
Harvey Withford-Cave <harvey.withfordcave@students.mq.edu.au>
2025-08-27 09:27:38 +1000

    Completed first attempt at exercise 1.

--- A1_ex1.R
+++ A1_ex1.R
@@ -2,5 +2,31 @@
 
 ## Q1
 
-X <- as.tibble() |> 
-  read.csv(vic_elect.csv)
+X <- read.csv("vic_elect.csv") |> 
+  as.tibble()
+
+## Q2
+
+str(X)
+
+## Q3
+
+sum(X$demand)
+
+## Q4
+
+X |> 
+  filter(month == "Jul") |> 
+  mean()
+
+## Q5
+
+X |> 
+  filter(temp < 15) |> 
+  median()
+
+## Q6
+
+X |>
+  arrange(desc(demand)) |> 
+  head(1)
