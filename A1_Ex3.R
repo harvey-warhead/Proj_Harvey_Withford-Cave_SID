29777cb4ba6eb5a2c66118b382a7cccf73fb874b
Harvey Withford-Cave <harvey.withfordcave@students.mq.edu.au>
2025-08-29 21:00:58 +1000

    Distinction level commit. Introduced mytest() function.

--- A1_Ex3.R
+++ A1_Ex3.R
@@ -1,60 +1,107 @@
 # Exercise 3
 
+## mytest function
+
+mytest <- function() {
+  setup()
+  csv_files <- list("A1_Ex3_not_signif.csv", "A1_Ex3_signif.csv")
+  for(file in csv_files) {
+    file_name <- read_csv(file, show_col_types = 0)
+    readline(prompt="Press [enter] to continue")
+    hypothesis(file_name)
+    print(assumptions(file_name))
+    fit1 <- fit(file_name)
+    decision(fit1)
+    conclusion(fit1)
+  }
+}
+
 ## Setup
 
-library(tidyverse)
-library(patchwork)
-
-data_sig <- read.csv("A1_Ex3_signif.csv")
-data_nsig <- read.csv("A1_Ex3_not_signif.csv")
+setup <- function() {
+  library(tidyverse)
+  library(patchwork)
+  library(broom)
+  library(mycor)
+}
 
 ## 1 Hypothesis
 
 hypothesis <- function(file_name) {
-  cat("Hypothesis for", deparse(substitute(file_name)), "\nH_0: β = 0 i.e. a linear model does not fit the data, against\nH_1: β ≠ 0 i.e. a linear model does fit the data")
+  return(cat("Hypothesis for", deparse(substitute(file_name)), "\nH_0: β = 0 i.e. a linear model does not fit the data, against\nH_1: β ≠ 0 i.e. a linear model does fit the data\n"))
 }
 
 ## 2 Assumptions
 
 assumptions <- function(file_name) {
-  model <- lm(file_name)
+  model <- lm(Y~X, data = file_name)
   fit <- fitted(model)
   res <- residuals(model)
   results <- data.frame(fitted_vals = fit, residuals = res)
+
+  p1 <- file_name |> 
+    ggplot(aes(x = X, y = Y)) +
+    geom_point() +
+    geom_smooth(method = "lm")
+  
+  p2 <- ggplot(data = results, aes(x = fitted_vals, y = residuals)) +
+    geom_point() +
+    geom_hline(yintercept = 0, colour = "blue", linetype = "dashed", linewidth = 0.7) +
+    labs(x = "fitted values")
+  
+  p3 <- ggplot(results, aes(x = residuals)) +
+    geom_histogram()
+  
+  out <- p1 / (p2 + p3)
+  
+  return(out)
 }
-
-
-p1 <- A1_Ex3_signif |> 
-  ggplot(aes(x = X, y = Y)) +
-  geom_point() +
-  geom_smooth(method = "lm")
-
-p2 <- ggplot(data = results, aes(x = fitted_vals, y = residuals)) +
-  geom_point() +
-  geom_hline(yintercept = 0, aes(colour = "blue"))
-
-p3 <- ggplot(results, aes(x = residuals)) +
-  geom_histogram()
-
-p1 / (p2 + p3)
 
 ## 3 Linear regression report
 
-lm(A1_Ex)
+fit <- function(file_name) {
+  model <- lm(Y~X, data = file_name)
+  
+  slope_est <- coef(model)["X"] # (β̂̂) slope estimate
+  confint <- confint(model, parm = "X", level = 0.95) #  95% confidence interval
+  t_val <- summary(model)$coefficients["X", "t value"]  # t statistic
+  df <- model$df.residual # df
+  p_val <- summary(model)$coefficients["X", "Pr(>|t|)"] #glance(model)[["p.value"]] # P value
 
-the_model <- lm(Y~X, data = A1_Ex3_signif)
-
-sum <- summary.lm(the_model)
-
-coef(the_model)["X"] # the slope estimate
-confint(the_model, parm = "X") #[1,2]  the confidence interval for the slope default level is .95
-summary(the_model)$coefficients["X", "t value"]  # t value for the slope
-the_model$df.residual #the degrees of freedom
-summary(the_model)$coefficients["X", "Pr(>|t|)"] # the pvalue
+  out <- list(
+    slope_est,
+    confint,
+    t_val,
+    df,
+    p_val
+  )
+  
+  cat("Slope estimate:", slope_est,
+      "\nConfidence interval (95%):", sprintf("[%.6f, %.6f]", confint[1], confint[2]),
+      "\nt statistic:", t_val,
+      "\nDegrees of freedom:", df,
+      "\nP value:", p_val)
+  cat("\nP value:", p_val)
+  
+  return(out)
+}
 
 ## 4 Decision
 
+decision <- function(fit1) {
+  if(p_val < 0.05) {
+    dec <- "reject"
+  }
+  else {
+    dex <- "not reject"
+  }
+  cat(dec)
+}
+
 ## 5 Conclusion
+  
+conclusion <- function(fit1) {
+  
+}
 
 # Reject the null hypothesis
-
